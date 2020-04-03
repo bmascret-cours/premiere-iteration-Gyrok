2eacd73ef3287bd4c0e000afb15130b630d0ba88
Gyrok <45562652+Gyrok@users.noreply.github.com>
2020-04-03 17:47:43 +0200

    AbsPiece ajout 1
    Ajout de quelques fonctions

--- src/model/AbstractPiece.java
+++ src/model/AbstractPiece.java
@@ -1,5 +1,13 @@
 package model;
 
 public abstract class AbstractPiece implements Pieces{
-
+	
+	public AbstractPiece(Couleur couleur,Coord coord) {
+		
+		
+	}
+	public boolean capture() {
+		
+		return false;
+	}
 }
