2362893c5d97efc8bbbe9524b828389f4e4d886e
Gyrok <45562652+Gyrok@users.noreply.github.com>
2020-04-21 19:24:43 +0200

    Update Jeu.java

--- src/model/Jeu.java
+++ src/model/Jeu.java
@@ -12,4 +12,26 @@ public class Jeu {
 		this.pieces = ChessPiecesFactory.newPieces(pieceCouleur);
 	}
 
+	// Test main
+	public static void main(String[] args) {
+		Jeu testConstruction = new Jeu(Couleur.NOIR);
+		testConstruction.toString();
+	}
+	
+	public void toString(List<Pieces> pieces) {
+		// itérer sur les pieces pour call les toString
+	}
+	public boolean isPieceHere(int x,int y) {
+		// boucle sur les 2 listes de pièces (?)
+		return true; // true si une pièce se trouve aux coordonnées indiquées
+	}
+	
+	public boolean isMoveOk(int xInit, int yInit,int xFinal,int yFinal) {
+		return true; // true si déplacement possible
+	}
+	
+	public boolean move(int xInit,int yInit,int xFinal,int yFinal) {
+		// appelle is moveOk puis collision
+		return true; // true si déplacement effectué
+	}
 }
