8da0161cb3ad438b45d0a79d25dd6cdd99dcf82e
Gyrok <45562652+Gyrok@users.noreply.github.com>
2020-04-21 19:44:24 +0200

    jeu écriture toString
    prb avec construction, factory problem
    pour générer la liste des pièces

--- src/model/Jeu.java
+++ src/model/Jeu.java
@@ -8,18 +8,24 @@ public class Jeu {
 	
 	List<Pieces> pieces;
 	public Jeu(Couleur couleur) {
-		Couleur pieceCouleur = couleur;
-		this.pieces = ChessPiecesFactory.newPieces(pieceCouleur);
+		//Couleur pieceCouleur = couleur;
+		//this.pieces = ChessPiecesFactory.newPieces(pieceCouleur);
 	}
 
 	// Test main
 	public static void main(String[] args) {
 		Jeu testConstruction = new Jeu(Couleur.NOIR);
-		testConstruction.toString();
+		List<Pieces> listPieces = ChessPiecesFactory.newPieces(Couleur.NOIR);
+		testConstruction.toString(listPieces);
 	}
 	
 	public void toString(List<Pieces> pieces) {
 		// itérer sur les pieces pour call les toString
+
+		for (Pieces piece : this.pieces) {
+		   String testString = piece.toString(); 
+		   System.out.println(testString);
+		}
 	}
 	public boolean isPieceHere(int x,int y) {
 		// boucle sur les 2 listes de pièces (?)
