aa0047b0e985c67884c5d666c965362b8f3eb6a9
Gyrok <45562652+Gyrok@users.noreply.github.com>
2020-04-23 18:06:15 +0200

    ajout de méthode pour Echiquier

--- src/model/Echiquier.java
+++ src/model/Echiquier.java
@@ -1,15 +1,42 @@
 package model;
 
 public class Echiquier implements BoardGames{
+	
+	// On initialise les 2 jeux.
+	private Jeu jeuCourant = null;
+	private Jeu jeuAttente = null;
+	
+	// Message
+	private String message = "message placeholder";
+	
+	public Echiquier() {
+		this.jeuCourant = new Jeu(Couleur.BLANC);
+		this.jeuAttente = new Jeu(Couleur.NOIR);
+	}
+	
+	/**
+	 * @param message the message to set
+	 */
+	private void setMessage(String message) {
+		this.message = message;
+	}
 
+	@Override
+	public String getMessage() {
+		return this.message;
+	}
+	
 	public Object getPiecesIHM() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public void switchJoueur() {
-		// TODO Auto-generated method stub
 		
+		// On échange les 2 jeux
+		Jeu tempStorage = this.jeuCourant;
+		this.jeuCourant = this.jeuAttente;
+		this.jeuAttente = tempStorage;
 	}
 
 	@Override
@@ -22,12 +49,6 @@ public class Echiquier implements BoardGames{
 	public boolean isEnd() {
 		// TODO Auto-generated method stub
 		return false;
-	}
-
-	@Override
-	public String getMessage() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 	@Override
