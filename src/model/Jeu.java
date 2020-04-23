6d53d34c7678a46745088a36808e918917a21289
Gyrok <45562652+Gyrok@users.noreply.github.com>
2020-04-23 17:36:28 +0200

    ajout de getPiecesIHM

--- src/model/Jeu.java
+++ src/model/Jeu.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import tools.ChessPiecesFactory;
@@ -113,7 +114,37 @@ public class Jeu {
 		this.move(this.xFinalPrevious, this.yFinalPrevious, this.xInitPrevious, this.yInitPrevious);
 	}
 
-	public java.util.List<PieceIHM> getPiecesIHM(){
-		return null; // TODO: trouver annexe
+	/**
+	* @return une vue de la liste des pièces en cours
+	* ne donnant que des accès en lecture sur des PieceIHM
+	* (type piece + couleur + liste de coordonnées)
+	*/
+	public List<PieceIHM> getPiecesIHM(){
+	PieceIHM newPieceIHM = null;
+	List<PieceIHM> list = new LinkedList<PieceIHM>();
+	for (Pieces piece : pieces){
+		boolean existe = false;
+		// si le type de piece existe déjà dans la liste de PieceIHM
+		// ajout des coordonnées de la pièce dans la liste de Coord de ce type
+		// si elle est toujours en jeu (x et y != -1)
+		for ( PieceIHM pieceIHM : list){
+			if ((pieceIHM.getTypePiece()).equals(piece.getClass().getSimpleName())){
+				existe = true;
+				if (piece.getX() != -1){
+					pieceIHM.add(new Coord(piece.getX(), piece.getY()));
+				}
+			}
+		}
+		// sinon, création d'une nouvelle PieceIHM si la pièce est toujours en jeu
+		if (! existe) {
+			if (piece.getX() != -1){
+				newPieceIHM = new PieceIHM(piece.getClass().getSimpleName(),
+						piece.getCouleur());
+						newPieceIHM.add(new Coord(piece.getX(), piece.getY()));
+						list.add(newPieceIHM);
+			}
+		}
+	}
+	return list;
 	}
 }
