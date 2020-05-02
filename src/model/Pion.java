1360c7c1ea791f61951eebcfdc2e9b4d882b280c
Gyrok <45562652+Gyrok@users.noreply.github.com>
2020-05-02 14:18:40 +0200

    correctif déplacement pion

--- src/model/Pion.java
+++ src/model/Pion.java
@@ -17,30 +17,74 @@ public class Pion extends AbstractPiece{
 			return false;
 		}
 		else {
-			if ((xFinal == this.x) && (yFinal == this.y - 1)) {
-
-				return true;
+			if (this.couleur == Couleur.BLANC) {
+				if ((xFinal == this.x) && (yFinal == this.y - 1)) {
+	
+					return true;
+				}
+				
+				// pour le saut possible au départ => implique que y = hauteur plateau
+				else if ((xFinal == this.x) && (yFinal == 4)) {
+					return true;
+				}
+				else {
+					return this.isMoveDiagOk(xFinal, yFinal);
+				}
 			}
-			
-			// pour le saut possible au départ => implique que y = hauteur plateau
-			else if ((xFinal == this.x) && (yFinal == 4)) {
-				return true;
+			else if (this.couleur == Couleur.NOIR) {
+				if ((xFinal == this.x) && (yFinal == this.y + 1)) {
+					
+					return true;
+				}
+				
+				// pour le saut possible au départ => implique que y = hauteur plateau
+				else if ((xFinal == this.x) && (yFinal == 3)) {
+					return true;
+				}
+				else {
+					return this.isMoveDiagOk(xFinal, yFinal);
+				}
 			}
 			else {
 				return false;
 			}
 		}
 	}
+	
 	public boolean isMoveDiagOk(int xFinal, int yFinal) {
 		if (this.OoB(xFinal, yFinal)){
 			return false;
 		}
 		else {
-			return true;
+			if (this.couleur == Couleur.BLANC) {
+				if ((xFinal == this.x + 1) && (yFinal == this.y - 1)) {
+	
+					return true;
+				}
+				else if ((xFinal == this.x - 1) && (yFinal == this.y - 1)) {
+					
+					return true;
+				}
+			}
+			else if (this.couleur == Couleur.NOIR) {
+				if ((xFinal == this.x + 1) && (yFinal == this.y + 1)) {
+	
+					return true;
+				}
+				else if ((xFinal == this.x - 1) && (yFinal == this.y + 1)) {
+					
+					return true;
+				}
+			}
+			else {
+				return false;
+			}
+				
+				
 		}
+		return false;
 	}
 }
-
 
 
           
